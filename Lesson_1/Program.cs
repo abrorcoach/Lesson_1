8f88e36fa35f85f6d8b723310eda39081823b9b7
Abror <abrorhalimov660@gmail.com>
2025-02-22 16:45:52 +0500

    a

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -147,7 +147,7 @@ internal class Program
 
         /////////////////////// 32)
 
-        //Console.WriteLine(GetObjectFromClass.IfHaveThreeTrue("1232"));
+        //Console.WriteLine(GetObjectFromClass.IfHaveThreeTrue("4332"));
 
         /////////////////////////// 33)
 
