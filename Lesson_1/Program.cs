99eefe30b576b4db5f51e9f940bf7e2792cf5236
Abror <abrorhalimov660@gmail.com>
2025-02-22 16:52:32 +0500

    a

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -167,7 +167,7 @@ internal class Program
 
         //////////////////////// 36)
 
-        //GetObjectFromClass.FindEqualABC(2, 4, 4);
+        //GetObjectFromClass.FindEqualABC(2, 6, 4);
 
         /////////////////// 37)
 
