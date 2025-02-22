f133124079a95ac5e08044ba2d64e84bd5c2c930
Abror <abrorhalimov660@gmail.com>
2025-02-22 16:24:48 +0500

    Add project files.

---
+++ Lesson_1/Class1.cs
@@ -0,0 +1,413 @@
+﻿namespace Lesson_1
+{
+    public class Class1
+    {
+        public int ArifMath(int a, int b, int c)
+        {
+            return (a + b + c) / 3;
+        }
+
+        public int Perimeter(int a)
+        {
+            return 4 * a;
+        }
+
+        public void Kvad(int a, int b)
+        {
+            Console.WriteLine($" S: {a * b}");
+            Console.WriteLine($" P: {2 * (a + b)}");
+        }
+
+        public void Kub(int a)
+        {
+            Console.WriteLine($" V: {a * a * a}");
+            Console.WriteLine($" S: {6 * a * a}");
+        }
+
+        public void Pow(int number)
+        {
+            Console.WriteLine($"{Math.Pow(number, 2)}");
+        }
+
+        public double Exchange(double number)
+        {
+            return number / 11200;
+        }
+
+        public int FirstThirdNumber(int number)
+        {
+            int first = number / 1000 % 100;
+            int third = number / 10 % 10;
+            return first * third;
+        }
+
+        public int FindThird(int number)
+        {
+            return number / 10 % 10;
+        }
+
+        public void PowSeven(int number)
+        {
+            Console.WriteLine(Math.Pow(number, 7));
+        }
+
+        public double SantimetersToMetr(double number)
+        {
+            return number * 0.01;
+        }
+
+        public void ex(string num)
+        {
+            string res = num.Substring(0, 1);
+            string res2 = num.Substring(1, 2);
+            Console.WriteLine(res2 + res);
+        }
+
+        public int ReverseNumber(int num)
+        {
+            int digitFirst = num % 10;
+            int digitSecond = (num / 10) % 10;
+            int digitThird = num / 100;
+
+            int res = digitFirst * 100 + digitSecond * 10 + digitThird;
+            return res;
+        }
+
+        public int FourthDigit(int num)
+        {
+            int numFirst = num / 1000;
+            int numSecond = (num / 100) % 10;
+            int numThird = (num / 10) % 10;
+            int numFourth = num % 10;
+
+            int res = numFirst * numSecond * numThird * numFourth;
+            return res;
+        }
+
+        public int AddNum(int num)
+        {
+            int digitFirst = num % 10;
+            int digitSecond = (num / 10) % 10;
+            int digitThird = num / 100;
+
+            int res = digitFirst + digitSecond + digitThird;
+            return res;
+        }
+
+        public double ByteToKilobyte(double num)
+        {
+            return num * 0.001;
+        }
+
+        public void SecundToHoursAndMinutes(int num)
+        {
+            double hours = num / 3600;
+            double minutes = (num % 3600) / 60;
+            Console.WriteLine($"hours: {hours}, minutes: {minutes}");
+
+        }
+
+        public void ChangeNumber(int a, int b)
+        {
+            Console.WriteLine($"A:{b}, B: {a}");
+        }
+
+        public void HigherThen999(int num)
+        {
+            if (num > 999 || true)
+            {
+                int numSecond = (num / 100) % 10;
+                Console.WriteLine(numSecond);
+            }
+
+
+        }
+
+        public int ReverseNumberLastOne(int num)
+        {
+            int digitFirst = num / 100;
+            int digitSecond = (num / 10) % 10;
+            int digitThird = num % 10;
+
+            int res = digitThird * 100 + digitFirst * 10 + digitSecond;
+            return res;
+        }
+
+        public void HoursToMinut(int numSutka, int numHours)
+        {
+            int res1 = numSutka * 1440;
+            int res2 = numHours * 60;
+
+            int answ = res1 + res2;
+            Console.WriteLine($"Minut: {answ}");
+        }
+
+        public double FindKm(double num)
+        {
+            double res = num / 7;
+            return res;
+
+        }
+
+        public void WeakToHours(int numWeak, int numDay)
+        {
+            int res1 = numWeak * 168;
+            int res2 = numDay * 24;
+
+            int answ = res1 + res2;
+            Console.WriteLine($"Hours: {answ}");
+        }
+
+        public void GbToKb(double numGb, double numKb)
+        {
+            double fileSizeKB = numGb * 1024 * 1024;
+            double timeInSeconds = fileSizeKB / numKb;
+
+            Console.WriteLine($"{numGb} GB ma'lumotni uzatish uchun {timeInSeconds} sekund kerak.");
+        }
+
+        public double SecundToHours(double numSec)
+        {
+            numSec = numSec / 3600;
+            return numSec;
+        }
+
+        public int FindMultipleFromThird(int num)
+
+        {
+            int digitFirst = num / 100;
+            int digitSecond = (num / 10) % 10;
+            int digitThird = num % 10;
+
+            int res = digitThird * digitFirst * digitSecond;
+            return res;
+        }
+
+        public void FindLenght(double numR)
+        {
+            numR = numR * 2 * 3.14;
+            Console.WriteLine($"Lenght: {numR} ");
+
+        }
+
+        public void FindByteFromKbyte(double num)
+        {
+            num = num * 1024;
+            double res = num / 2;
+            Console.WriteLine($"we use {res} byte");
+        }
+
+        public int OnXonNumTurnLeft(int num)
+        {
+            int digitFirst = num / 100;
+            int digitSecond = (num / 10) % 10;
+            int digitThird = num % 10;
+
+            int res = digitSecond * 100 + digitFirst * 10 + digitThird;
+            return res;
+        }
+
+        public bool IfHaveThreeTrue(string num)
+        {
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] == '3')
+                {
+                    return true;
+
+                }
+
+            }
+            return false;
+        }
+
+        public void FindPositiveNegativeNum(int[] num)
+        {
+            int positive = 0;
+            int negative = 0;
+            int zero = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] > 0)
+                {
+                    positive++;
+                }
+                else if (num[i] < 0)
+                {
+                    negative++;
+                }
+                else
+                {
+                    zero++;
+                }
+
+            }
+
+            Console.WriteLine($"Postive: {positive}");
+            Console.WriteLine($"Negative: {negative}");
+            Console.WriteLine($"Zero: {zero}");
+        }
+
+        public void FindMonth(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Januare");
+                    break;
+                case 2:
+                    Console.WriteLine("Februare");
+                    break;
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+                case 4:
+                    Console.WriteLine("Aprel");
+                    break;
+                case 5:
+                    Console.WriteLine("May");
+                    break;
+                case 6:
+                    Console.WriteLine("Ilun");
+                    break;
+                case 7:
+                    Console.WriteLine("Ilul");
+                    break;
+                case 8:
+                    Console.WriteLine("August");
+                    break;
+                case 9:
+                    Console.WriteLine("Sentabr");
+                    break;
+                case 10:
+                    Console.WriteLine("October");
+                    break;
+                case 11:
+                    Console.WriteLine("Noverber");
+                    break;
+                case 12:
+                    Console.WriteLine("December");
+                    break;
+
+                default:
+                    Console.WriteLine("we dont have thi kind of moth");
+                    break;
+
+            }
+        }
+
+        public void Grades(int num)
+        {
+            if (num >= 0 && num <= 54)
+            {
+                Console.WriteLine("You get 2 score");
+            }
+            else if (num >= 55 && num <= 70)
+            {
+                Console.WriteLine("You get 3 score");
+            }
+            else if (num >= 71 && num <= 84)
+            {
+                Console.WriteLine("You get 4 score");
+            }
+            else if (num >= 71 && num <= 100)
+            {
+                Console.WriteLine("You get 5 score");
+            }
+            else
+            {
+                Console.WriteLine("it too high");
+            }
+
+        }
+
+        public void FindEqualABC(int a, int b, int c)
+        {
+            if (a == b && b == c)
+            {
+                Console.WriteLine("=");
+            }
+            else if (a == b)
+            {
+                Console.WriteLine(c);
+            }
+            else if (a == c)
+            {
+                Console.WriteLine(b);
+            }
+            else if (b == c)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine($"{a} {b} {c}");
+            }
+
+        }
+
+        public void AddTwoNum(int a, int b)
+        {
+            int res = a + b;
+
+            if (res >= 10 && res <= 19)
+            {
+                Console.WriteLine("20");
+            }
+            else
+            {
+                Console.WriteLine(res);
+            }
+        }
+
+        public void FindThreeInDigit(int num)
+        {
+
+            int numSecond = (num / 100) % 10;
+            if (numSecond == 3)
+            {
+                Console.WriteLine("There is 3 ");
+            }
+            else
+            {
+                Console.WriteLine("there isnt  3 ");
+            }
+
+
+
+        }
+
+        public void MultipleFiveTime(int num)
+        {
+            if (num > 0)
+            {
+                Console.WriteLine(num * 15);
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine(num * -1);
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+
+        public void AddOneTwo(int a)
+        {
+            if (a > 0)
+            {
+                Console.WriteLine(a + 1);
+            }
+            else if (a < 0)
+            {
+                Console.WriteLine(Math.Abs(a) + 2);
+            }
+            else
+            {
+                Console.WriteLine(a / 100);
+            }
+        }
+
+    }
+}
---
+++ Lesson_1/Program.cs
@@ -0,0 +1,191 @@
+﻿using System.Net.WebSockets;
+
+namespace Lesson_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        var GetObjectFromClass = new Class1();
+
+        ////////////////// 1)
+
+        //Console.WriteLine(GetObjectFromClass.ArifMath(12, 15, 21));
+        /////////////////// 2)
+
+        //Console.WriteLine($"P :{GetObjectFromClass.Perimeter(12)}");
+        ///////////////////// 3)
+
+        //GetObjectFromClass.Kvad(32, 12);
+
+        ///////////////////// 4)
+
+        //GetObjectFromClass.Kub(6);
+
+        //////////////////////// 6)
+
+        //GetObjectFromClass.Pow(5);
+
+        //////////////////////////////// 7)
+
+        //Console.WriteLine($" Dollar: {GetObjectFromClass.Exchange(20000)}");
+
+        ////////////////////////// 8)
+
+        //Console.WriteLine($" Output{GetObjectFromClass.FirstThirdNumber(1234)}");
+
+        ///////////////////////// 9)
+
+        //Console.WriteLine($"Output: {GetObjectFromClass.FindThird(3241)}");
+
+        ////////////////////////// 10)
+
+        //GetObjectFromClass.PowSeven(2);
+
+        ///////////////////////////// 11)
+
+        //Console.WriteLine("Enter sentimeters to get metrs");
+        //double num = double.Parse(Console.ReadLine());
+        //Console.WriteLine(GetObjectFromClass.SantimetersToMetr(num));
+
+        /////////////////////// 13)
+
+        //GetObjectFromClass.ex("123");
+
+        ///////////////////////// 14)
+
+        //Console.WriteLine(GetObjectFromClass.ReverseNumber(123));
+
+        /////////////////////// 15)
+
+        //Console.WriteLine(GetObjectFromClass.FourthDigit(1234));
+
+        ////////////////////// 16)
+
+        //Console.WriteLine(GetObjectFromClass.AddNum(435));
+
+        //////////////////// 17)
+
+        //Console.WriteLine("Enter byte to get kilobyte");
+        //double num = double.Parse(Console.ReadLine());
+        //Console.WriteLine(GetObjectFromClass.ByteToKilobyte(num));
+
+        /////////////////// 18)
+
+        //GetObjectFromClass.SecundToHoursAndMinutes(3600);
+
+        ///////////////// 19)
+
+        //Console.WriteLine("Enter First Number A:");
+        //int num1 = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter Second Number B:");
+        //int num2 = int.Parse(Console.ReadLine());
+        //GetObjectFromClass.ChangeNumber(num1,num2);
+
+        //////////////////////// 20)
+
+        //GetObjectFromClass.HigherThen999(-3241);
+
+        ////////////////////////// 21)
+
+        //Console.WriteLine(GetObjectFromClass.ReverseNumberLastOne(473));
+
+        ///////////////////////// 22)
+
+        //Console.WriteLine("Enter Sutka:");
+        //int num1 = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter Hours:");
+        //int num2 = int.Parse(Console.ReadLine());
+        //GetObjectFromClass.HoursToMinut(num1, num2);
+
+        /////////////////////// 23)
+
+        //Console.WriteLine("Enter Country km");
+        //double num = double.Parse(Console.ReadLine());
+        //Console.WriteLine($" You have to walk {GetObjectFromClass.FindKm(num)} hours to get to city");
+
+        ///////////////////////// 24)
+
+        //Console.WriteLine("Enter weak:");
+        //int num1 = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter day:");
+        //int num2 = int.Parse(Console.ReadLine());
+        //GetObjectFromClass.WeakToHours(num1, num2);
+
+        ////////////////////////////// 25)
+
+
+        //GetObjectFromClass.GbToKb(1.8, 750);
+
+
+        /////////////////////////////// 26)
+
+        //Console.WriteLine("Enter sec to get Hour");
+        //double num = double.Parse(Console.ReadLine());
+        //Console.WriteLine(GetObjectFromClass.SecundToHours(num));
+
+        /////////////////////////////// 28)
+
+        //Console.WriteLine(GetObjectFromClass.FindMultipleFromThird(123));
+
+        /////////////////////// 29)
+
+        //Console.WriteLine("Enter Radius to find lenght");
+        //double num = double.Parse(Console.ReadLine());
+        //GetObjectFromClass.FindLenght(num);
+
+        ////////////////////////// 30)
+
+        //Console.WriteLine("Enter file size");
+        //double num = double.Parse(Console.ReadLine());
+        //GetObjectFromClass.FindByteFromKbyte(num);
+
+        /////////////////////////////// 31)
+
+        //Console.WriteLine(GetObjectFromClass.OnXonNumTurnLeft(358));
+
+        /////////////////////// 32)
+
+        //Console.WriteLine(GetObjectFromClass.IfHaveThreeTrue("1232"));
+
+        /////////////////////////// 33)
+
+
+        //GetObjectFromClass.FindPositiveNegativeNum(new[] { 3, -2, 12, 0, 12 });
+
+
+        //////////////////////////// 34)
+
+        //GetObjectFromClass.FindMonth(1);
+
+        ///////////////////// 35)
+
+        //Console.WriteLine("Enter your grade");
+        //int num = int.Parse(Console.ReadLine());
+        //GetObjectFromClass.Grades(num);
+
+        //////////////////////// 36)
+
+        //GetObjectFromClass.FindEqualABC(2, 4, 4);
+
+        /////////////////// 37)
+
+        //GetObjectFromClass.AddTwoNum(4,7);
+
+        //////////////////////// 38)
+
+        //GetObjectFromClass.FindThreeInDigit(1324);
+
+        /////////////////////////// 39)
+
+        //GetObjectFromClass.MultipleFiveTime(4);
+
+        /////////////////////////// 40)
+
+        //GetObjectFromClass.AddOneTwo(0);
+        
+    }
+
+}
+
